e4d9030f0e5e35333e01db86aa8b9e4dcf63ec86
Jesse Glick <jglick@cloudbees.com>
2016-06-10 10:08:28 -0400

    Use exponential backoff for logging in TryRepeatedly. The actual attempts still happen at a regular interval.

--- src/main/java/org/jenkinsci/plugins/workflow/support/pickles/TryRepeatedly.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/pickles/TryRepeatedly.java
@@ -26,6 +26,7 @@ package org.jenkinsci.plugins.workflow.support.pickles;
 
 import com.google.common.util.concurrent.AbstractFuture;
 import com.google.common.util.concurrent.ListenableFuture;
+import hudson.console.ModelHyperlinkNote;
 import hudson.model.TaskListener;
 import java.io.IOException;
 import jenkins.util.Timer;
@@ -37,11 +38,11 @@ import java.util.logging.Logger;
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
 import org.jenkinsci.plugins.workflow.flow.FlowExecutionOwner;
+import org.jenkinsci.plugins.workflow.pickles.Pickle;
 
 /**
  * {@link ListenableFuture} that promises a value that needs to be periodically tried.
- *
- * @author Kohsuke Kawaguchi
+ * Specialized for use from {@link Pickle#rehydrate(FlowExecutionOwner)}.
  */
 public abstract class TryRepeatedly<V> extends AbstractFuture<V> {
 
@@ -49,6 +50,12 @@ public abstract class TryRepeatedly<V> extends AbstractFuture<V> {
 
     private final int delay;
     private ScheduledFuture<?> next;
+    /** Number of {@link #tryLater} calls to run between logging attempts. */
+    private float backoff = 1;
+    /** Amount by which {@link #backoff} gets multiplied, so we do not flood the log with endless messages. */
+    private static final float BACKOFF_EXPONENT = 1.42f; // >√2
+    /** Reset to {@link #backoff} after each message, then decremented by on each call to {@link #tryLater}. */
+    private int retriesRemaining;
 
     protected TryRepeatedly(int delay) {
         this(delay, delay);
@@ -59,17 +66,22 @@ public abstract class TryRepeatedly<V> extends AbstractFuture<V> {
         tryLater(initialDelay);
     }
 
+    /**
+     * Override to supply the owner passed to {@link Pickle#rehydrate(FlowExecutionOwner)}.
+     */
     protected @Nonnull FlowExecutionOwner getOwner() {
         return FlowExecutionOwner.dummyOwner();
     }
 
+    /**
+     * Assuming {@link #getOwner} has been overridden, override to print a message to the build log explaining why the pickle is still unloadable.
+     * Could use {@link ModelHyperlinkNote} etc.
+     */
     protected void printWaitingMessage(@Nonnull TaskListener listener) {
         listener.getLogger().println("Still trying to load " + this);
     }
 
     private void tryLater(int currentDelay) {
-        // TODO log a warning if trying for too long; probably Pickle.rehydrate should be given a TaskListener to note progress
-
         if (isCancelled())      return;
 
         next = Timer.get().schedule(new Runnable() {
@@ -78,11 +90,16 @@ public abstract class TryRepeatedly<V> extends AbstractFuture<V> {
                 try {
                     V v = tryResolve();
                     if (v == null) {
-                        try {
-                            // TODO do not print a message every second; back off exponentially
-                            printWaitingMessage(getOwner().getListener());
-                        } catch (IOException x) {
-                            LOGGER.log(Level.WARNING, null, x);
+                        if (retriesRemaining == 0) {
+                            try {
+                                printWaitingMessage(getOwner().getListener());
+                            } catch (IOException x) {
+                                LOGGER.log(Level.WARNING, null, x);
+                            }
+                            backoff *= BACKOFF_EXPONENT;
+                            retriesRemaining = (int) backoff;
+                        } else {
+                            retriesRemaining--;
                         }
                         tryLater(delay);
                     } else {
