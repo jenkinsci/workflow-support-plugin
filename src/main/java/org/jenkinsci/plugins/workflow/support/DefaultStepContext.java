ae014c30f3bc2def547f3514b4346f4fd9c36680
Jesse Glick <jglick@cloudbees.com>
2024-11-08 17:06:10 -0500

    Javadoc https://github.com/jenkinsci/workflow-support-plugin/pull/252#issuecomment-1932637091

--- src/main/java/org/jenkinsci/plugins/workflow/support/DefaultStepContext.java
+++ src/main/java/org/jenkinsci/plugins/workflow/support/DefaultStepContext.java
@@ -69,6 +69,8 @@ public abstract class DefaultStepContext extends StepContext {
 
     /**
      * Uses {@link #doGet} but automatically translates certain kinds of objects into others.
+     * <p>Note that some basic types are handled directly by {@link #get} and cannot be overridden,
+     * such as {@link Run}, {@link Job}, {@link FlowExecution}, and {@link FlowNode}.
      * <p>{@inheritDoc}
      */
     @Override public final <T> T get(Class<T> key) throws IOException, InterruptedException {
@@ -78,7 +80,7 @@ public abstract class DefaultStepContext extends StepContext {
         } else if (Job.class.isAssignableFrom(key)) {
             value = castOrNull(key, get(Run.class).getParent());
         } else if (FlowExecution.class.isAssignableFrom(key)) {
-            value = castOrNull(key,getExecution());
+            value = castOrNull(key, getExecution());
         } else if (FlowNode.class.isAssignableFrom(key)) {
             value = castOrNull(key, getNode());
         }
