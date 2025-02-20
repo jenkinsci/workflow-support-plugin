c7247663a14203a42188fc8371ca3485da3ca33c
Jesse Glick <jglick@cloudbees.com>
2025-02-20 18:14:04 -0500

    Attempting to load outside Jenkins is not going well: `InvalidClassException: WorkflowScript; Class does not extend stream superclass`

--- src/test/java/org/jenkinsci/plugins/workflow/support/pickles/serialization/RiverReaderStackOverflowErrorDiag.java
+++ src/test/java/org/jenkinsci/plugins/workflow/support/pickles/serialization/RiverReaderStackOverflowErrorDiag.java
@@ -24,8 +24,16 @@
 
 package org.jenkinsci.plugins.workflow.support.pickles.serialization;
 
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+import java.io.DataInputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.Test;
+import org.jboss.marshalling.Marshalling;
+import org.jboss.marshalling.MarshallingConfiguration;
+import org.jboss.marshalling.SimpleClassResolver;
+import org.jboss.marshalling.river.RiverMarshallerFactory;
+import static org.jenkinsci.plugins.workflow.support.pickles.serialization.RiverReader.parseHeader;
 
 // mvnd test-compile exec:java -Dexec.mainClass=org.jenkinsci.plugins.workflow.support.pickles.serialization.RiverReaderStackOverflowErrorDiag -Dexec.classpathScope=test -Dexec.arguments=/path/to/program.dat
 public final class RiverReaderStackOverflowErrorDiag {
@@ -33,8 +41,19 @@ public final class RiverReaderStackOverflowErrorDiag {
     public static void main(String[] args) throws Exception {
         var programDat = Path.of(args[0]);
         // TODO probably need to specify a megawar also
-        // TODO parseHeader, readPickles, read main object
         System.err.println("Will load " + programDat.toRealPath());
+        Script script = new GroovyShell().parse("public class WorkflowScript extends Script implements Serializable {def run() {}}");
+        var classLoader = script.getClass().getClassLoader();
+        try (var in = Files.newInputStream(programDat)) {
+            var din = new DataInputStream(in);
+            parseHeader(din); // ignore offset, not bothering to read pickles
+            var config = new MarshallingConfiguration();
+            config.setClassResolver(new SimpleClassResolver(classLoader));
+            var eu = new RiverMarshallerFactory().createUnmarshaller(config);
+            eu.start(Marshalling.createByteInput(din));
+            var g = eu.readObject();
+            System.err.println("Loaded " + g);
+        }
     }
 
 }
