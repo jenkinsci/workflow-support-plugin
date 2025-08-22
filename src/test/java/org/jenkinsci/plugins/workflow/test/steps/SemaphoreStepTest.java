e898b2765ec9f88763d8e10a4d3dac796dc04831
Devin Nusbaum <dnusbaum@cloudbees.com>
2025-08-22 16:08:54 -0400

    Add BuildWatcher to SemaphoreStepTest and remove unnecessary future

--- src/test/java/org/jenkinsci/plugins/workflow/test/steps/SemaphoreStepTest.java
+++ src/test/java/org/jenkinsci/plugins/workflow/test/steps/SemaphoreStepTest.java
@@ -1,27 +1,26 @@
 package org.jenkinsci.plugins.workflow.test.steps;
 
 import hudson.model.Result;
-import hudson.model.queue.QueueTaskFuture;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.Rule;
 import org.junit.Test;
+import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
-
-import static org.junit.Assert.assertNotNull;
 
 public class SemaphoreStepTest {
     @Rule
     public JenkinsRule j = new JenkinsRule();
 
+    @Rule
+    public BuildWatcher watcher = new BuildWatcher();
+
     @Test
     public void hardKill() throws Exception {
         WorkflowJob p1 = j.jenkins.createProject(WorkflowJob.class, "p");
         p1.setDefinition(new CpsFlowDefinition("echo 'locked!'; semaphore 'wait'"));
-        QueueTaskFuture<WorkflowRun> future = p1.scheduleBuild2(0);
-        assertNotNull(future);
-        WorkflowRun b = future.waitForStart();
+        WorkflowRun b = p1.scheduleBuild2(0).waitForStart();
         SemaphoreStep.waitForStart("wait/1", b);
 
         b.doKill();
